664545e10c03eec97e388908af82ffb05d771fbc
Sofia Talarmo <sofia.talarmo@helsinki.fi>
2022-10-31 19:44:33 +0200

    Make requested changes, update tests for 100% coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,45 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_liikalisays_varastoon(self):
+        self.varasto.lisaa_varastoon(12)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_poisto_tyhjasta(self):
+        self.varasto.ota_varastosta(3)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10.0)
+
+    def test_liikapoisto(self):
+        self.varasto.lisaa_varastoon(5)
+        saatu = self.varasto.ota_varastosta(8)
+
+        self.assertAlmostEqual(saatu, 5)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10.0)
+
+    def test_to_string(self):
+        self.varasto.lisaa_varastoon(3)
+
+        self.assertEqual(str(self.varasto), "saldo = 3, vielä tilaa 7")
+
+    def test_init(self):
+        var = Varasto(10, 0)
+        self.assertAlmostEqual(var.paljonko_mahtuu(), 10.0)
+        var = Varasto(10, 11)
+        self.assertAlmostEqual(var.paljonko_mahtuu(), 0.0)
+        var = Varasto(10, 5)
+        self.assertAlmostEqual(var.paljonko_mahtuu(), 5.0)
+        var = Varasto(-1, 5)
+        self.assertAlmostEqual(var.tilavuus, 0.0)
+        var = Varasto(10, -1)
+        self.assertAlmostEqual(var.saldo, 0.0)
+
+    def test_invalid_lisays(self):
+        self.varasto.lisaa_varastoon(-10)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_invalid_otto(self):
+        self.varasto.ota_varastosta(-10)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
