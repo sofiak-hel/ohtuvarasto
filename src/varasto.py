dada01ca09c5ff4a9b6d9dacbb03e39c689af3c8
Sofia Talarmo <sofia.talarmo@helsinki.fi>
2022-11-07 21:34:36 +0200

    Fix accidental typo

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,6 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo=0):
-        self.tilavuus = max(0.0, self.tilavuus)
+        self.tilavuus = max(0.0, tilavuus)
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
